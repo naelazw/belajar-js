e26ab9282e6d9c1f3b74d86a95280f8699c8c87b
naelazw <naelazahwa487@gmail.com>
2024-11-05 21:59:35 +0700

    upload pertama

---
+++ dasar1.js
@@ -0,0 +1,66 @@
+// cara membuat variabel 
+var a = 'a';
+let b = 'b';
+const c ='c'
+
+
+//variabel bisa menampung tipe dat string (text), intejer , boolean, objeck dan array
+
+let nama = "naela" ;
+let umur ="18" ;
+let apakahBener = true;
+let uang = 1000.5 
+
+
+// Operator aritmatika 
+
+let angka1 = 20 ;
+let angka2 = 10;
+console.log(angka1+angka2, 'penjumlahan');
+console.log(angka1 - angka2, 'pengurangan');
+console.log(angka1 / angka2, 'pembagian');
+console.log(angka1 % angka2, 'sisa bagi');
+
+
+//Belajara mengisi variabel 
+let penampungOperatorAritmatika = angka1 +angka2;
+console.log(penampungOperatorAritmatika)
+
+penampungOperatorAritmatika = angka1 - angka2;
+console.log(penampungOperatorAritmatika)
+
+ penampungOperatorAritmatika = angka1 / angka2;
+console.log(penampungOperatorAritmatika)
+
+ penampungOperatorAritmatika = angka1 * angka2;
+console.log(penampungOperatorAritmatika)
+
+
+//String bisa ditambahkan namum string tiba bisa dikurangi dengan operator aritmatika
+let firstName = "naela";
+let lastName = "Zahwa";
+let fullName =(firstName + ' ' + lastName);
+console.log(fullName);
+console.log(firstName + " " + lastName);
+console.log(firstName - lastName);
+
+
+//alur code 
+// untuk baca kode dari kiri ke kanan dan atas ke bawah
+
+let number = 20 ;
+console.log (number + 10); //30
+number = 10;
+console.log (number + 10); //20
+number = false
+console.log (number + 10); //10
+
+
+//false = 0 dan tru = 1
+console.log(true + 10)
+
+
+//membaca eror 
+// const hewan = "jerapah"; //const konstan
+// console.log(hewan)
+// hewan = "buaya";
